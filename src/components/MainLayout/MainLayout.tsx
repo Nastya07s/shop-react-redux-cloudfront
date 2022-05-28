e9b14cfa6af2a66c84f7a9dc435f79b2b72852ad
Nastya07s <nastya.kotovich07@gmail.com>
2022-05-28 19:15:59 +0200

    feat: add deploy

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -8,9 +8,9 @@ import Header from "components/MainLayout/components/Header";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {'Copyright © Nastya07s   33 '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        Bookstore
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
