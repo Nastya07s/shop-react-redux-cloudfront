188eeb905a107eb9d54e335ad34523e738b32b2e
Nastya07s <nastya.kotovich07@gmail.com>
2022-06-13 04:06:47 +0300

    fix: cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com',
-  order: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com',
-  import: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com',
-  bff: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com',
-  cart: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com',
+  product: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com/dev',
+  order: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com/dev',
+  bff: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com/dev',
+  cart: 'https://5dv6abv1xh.execute-api.us-east-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
